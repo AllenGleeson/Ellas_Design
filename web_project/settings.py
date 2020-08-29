e8c3ede6c4bdb79af958748127d8d8dfdbc4aaf0
AllenGleeson <allen_gleeson@hotmail.com>
2020-08-29 21:49:48 +0100

    Fixing collecting static files

--- web_project/settings.py
+++ web_project/settings.py
@@ -194,8 +194,13 @@ AWS_DEFAULT_ACL = None
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_LOCATION = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 MEDIAFILES_LOCATION = 'media'
